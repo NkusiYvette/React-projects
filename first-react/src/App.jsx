0424cfe4b5200f50f9487543dafdbb6ba9c23b60
UWIMPAYE Yvette <nkusiyvette1@gmail.com>
2023-12-14 11:26:22 +0200

    react

---
+++ first-react/src/App.jsx
@@ -0,0 +1,24 @@
+
+import './App.css'
+import Component from './components/component'
+function App() {
+ 
+
+  return (
+    <>
+      <div>
+      <h1>My first project</h1>
+      </div>
+      <body>
+        <form action="">
+        <label htmlFor="">Name</label>
+        <input type="text" name='login' />
+        </form>
+       
+      </body>
+      <Component/>
+    </>
+  );
+}
+
+export default App
